a190b2eb7a3161c3ee7be881328cadcd78d414a9
Brent <brent.champion@queensu.ca>
2024-06-20 12:50:11 -0400

    add part 1 of lab 2

---
+++ Lab2/lab2_depth_imaging.py
@@ -0,0 +1,39 @@
+'''lab2_depth_imaging.py
+
+This walkthrough is to get used to interfacing 
+with the intelrealsense camera and test out its different modes
+
+'''
+import time
+import cv2
+from pal.products.qcar import QCarRealSense, IS_PHYSICAL_QCAR
+
+if not IS_PHYSICAL_QCAR:
+    import qlabs_setup
+    qlabs_setup.setup()
+
+
+#Initial Setup
+runTime = 30.0 # seconds
+max_distance = 2 # meters (for depth camera)
+
+with QCarRealSense('''              ''') as myCam:
+    t0 = time.time()
+    while time.time() - t0 < runTime:
+
+
+        #------ 1.1 TEST REALSENSE CAMERA ------#
+
+
+
+
+
+
+
+
+
+
+
+        #----------------------------------------#
+
+        cv2.waitKey(100)
